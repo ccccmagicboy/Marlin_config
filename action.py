66296c7b80162c6575380f01cee5a20894a42f5b
cccc <cuiwei_cv@163.com>
2020-05-17 16:46:40 +0800

    wakey wakey GitHub Actions

--- action.py
+++ action.py
@@ -3,7 +3,7 @@ import sys
 
 def send_cmd(command):
     print(command)
-    result_str = os.popen(command).read()
+    result_str = os.popen([command]).read()
     print(result_str)
     return result_str
 
@@ -12,6 +12,7 @@ print(os.listdir(os.getcwd()))
 
 sys.path.append('{0:s}/my_marlin/buildroot/bin'.format(os.getcwd()))
 print(sys.path)
+
 send_cmd('restore_configs')
 send_cmd('opt_set MOTHERBOARD {0:s}'.format(os.environ['BOARD']))
 print('The select board is {0:s}'.format(os.environ['BOARD']))
