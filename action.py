a40dd72e5d90e89e788faaf08bbe9afb298b574d
cccc <cuiwei_cv@163.com>
2020-05-17 17:47:53 +0800

    wakey wakey GitHub Actions

--- action.py
+++ action.py
@@ -4,6 +4,12 @@ import subprocess
 import shlex
 from termcolor import colored
 
+def replaceAll(file,searchExp,replaceExp):
+    for line in fileinput.input(file, inplace=1):
+        if searchExp in line:
+            line = line.replace(searchExp,replaceExp)
+        sys.stdout.write(line)
+
 def send_cmd(command):
     print(command)
     result_str =os.popen(command).read()
@@ -17,6 +23,8 @@ send_cmd('chmod +x {0:s}/my_marlin/buildroot/bin/*'.format(os.getcwd()))
 sys.path.append('{0:s}/my_marlin/buildroot/bin'.format(os.getcwd()))
 print(sys.path)
 os.chdir('my_marlin')
+
+# replaceAll(mk_path, "#webrepl.start()","import machine;".format(str))
 
 subprocess.call(['{0:s}/buildroot/bin/restore_configs'.format(os.getcwd())])
 subprocess.call(shlex.split('{0:s}/buildroot/bin/opt_set MOTHERBOARD {1:s}'.format(os.getcwd(), os.environ['BOARD'])))
